d321d1b4b28d4fa516cfb2a1fbf207b3f1230d86
Nick Adams <nadams@node-3.net>
2013-07-07 14:23:19 -0600

    Can now select users from the database

--- app/data/repository/UserRepository.scala
+++ app/data/repository/UserRepository.scala
@@ -1,14 +1,47 @@
 package data.repository
 
+import anorm._ 
+import play.api.db.DB
+import play.api.Play.current
+
 import data.entities._
 
 trait UserRepositoryComponent {
 	val userRepository = new UserRepository
 	
 	class UserRepository {
+		val table = "user"
+
 		val user = User(1, "test", "password", "")
 
-		def getUserById(id: Int) : Option[User] = Option(user)
-		def getUserByUsername(username: String) : Option[User] = Option(user)
+		def getUserById(id: Int) : Option[User] = {
+			DB.withConnection { implicit connection => 
+				SQL(
+					s"""
+						SELECT id, email, password, salt, dateCreated
+						FROM $table AS u
+						WHERE u.id = {id}
+					"""
+				).on("id" -> id)().collect {
+					case Row(id: Int, email: String, password: String, salt: String, dateCreated: java.sql.Timestamp) =>
+						User(id, email, password, salt)
+				}.headOption
+			}
+		}
+
+		def getUserByUsername(email: String) : Option[User] = {
+			DB.withConnection { implicit connection => 
+				SQL(
+					s"""
+						SELECT id, email, password, salt, dateCreated
+						FROM $table AS u
+						WHERE u.email = {email}
+					"""
+				).on("email" -> email)().collect {
+					case Row(id: Int, email: String, password: String, salt: String, dateCreated: java.sql.Timestamp) =>
+						User(id, email, password, salt)
+				}.headOption
+			}
+		}
 	}
 }
