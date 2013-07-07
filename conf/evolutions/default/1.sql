42f2232bce5db7066e594961d68bd86224eee9ce
nadams <nadams@node-3.net>
2013-07-07 13:22:44 -0600

    Creating user table

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,16 @@
+# user schema
+
+# --- !Ups
+CREATE TABLE user (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(64) NOT NULL UNIQUE,
+	password CHAR(64) NOT NULL,
+	salt CHAR(64) NOT NULL,
+	dateCreated DATETIME NOT NULL,
+
+	CONSTRAINT pk_user PRIMARY KEY(id) ,
+	CONSTRAINT uq_user_email UNIQUE(email)
+) ;
+
+# --- !Downs
+DROP TABLE user ;
