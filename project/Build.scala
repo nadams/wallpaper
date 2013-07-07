a2e14bea3b60d92300ab3b0add0ea7088ae441e0
nadams <nadams@node-3.net>
2013-07-07 12:28:16 -0600

    Adding mysql to project

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "mysql" % "mysql-connector-java" % "5.1.25"
   )
 
   val compassTask = TaskKey[Unit]("compass", "Compile sass")
@@ -22,5 +23,4 @@ object ApplicationBuild extends Build {
     compassTaskSettings,
     compile in Compile <<= (compile in Compile).dependsOn(compassTask)
   )
-
 }
