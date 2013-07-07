9400ec8d93f2cd4fa54d3191a5c33c4b3720aa0a
Nick Adams <nadams@node-3.net>
2013-07-06 21:25:26 -0600

    Filling in register page

--- app/models/profile/RegisterModel.scala
+++ app/models/profile/RegisterModel.scala
@@ -1,3 +1,11 @@
 package models.profile
 
-case class RegisterModel()
+case class RegisterModel(
+	username: Option[String], 
+	password: Option[String], 
+	passwordVerify: Option[String]
+)
+
+object RegisterModel {
+	def apply() : RegisterModel = RegisterModel(None, None, None)
+}
