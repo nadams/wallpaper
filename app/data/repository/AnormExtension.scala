4edf794cd76c247976c919c93f83d25a2377ed71
Nick Adams <nadams@node-3.net>
2013-07-09 06:56:30 -0600

    Now returning a datetime with the zone set to utc when selecting from the database

--- app/data/repository/AnormExtension.scala
+++ app/data/repository/AnormExtension.scala
@@ -10,8 +10,8 @@ object AnormExtension {
 	implicit def rowToDateTime: Column[DateTime] = Column.nonNull { (value, meta) =>
 		val MetaDataItem(qualified, nullable, clazz) = meta
 		value match {
-			case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime, DateTimeZone.UTC))
-			case d: java.sql.Date => Right(new DateTime(d.getTime, DateTimeZone.UTC))
+			case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime).withZoneRetainFields(DateTimeZone.UTC))
+			case d: java.sql.Date => Right(new DateTime(d.getTime).withZoneRetainFields(DateTimeZone.UTC))
 			case str: java.lang.String => Right(dateFormatGeneration.parseDateTime(str))  
 			case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass) )
 		}
