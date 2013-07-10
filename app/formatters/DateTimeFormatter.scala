9486019ebb84b4580792887808976f369d690b3a
Nick Adams <nadams@node-3.net>
2013-07-10 10:01:23 -0600

    Moving variable into block

--- app/formatters/DateTimeFormatter.scala
+++ app/formatters/DateTimeFormatter.scala
@@ -5,6 +5,8 @@ import org.joda.time.DateTime
 import play.api.libs.json._
 
 object DateTimeFormatter {
-	val format = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-	implicit val dateFormat = Format[DateTime](Reads.jodaDateReads(format), Writes.jodaDateWrites(format))
+	implicit val dateFormat = { 
+		val format = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+		Format[DateTime](Reads.jodaDateReads(format), Writes.jodaDateWrites(format))
+	}
 }
