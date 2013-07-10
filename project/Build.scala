2f482102e6ee369dace95cf9152285a4416164fe
Nick Adams <nadams@node-3.net>
2013-07-10 06:45:15 -0600

    Removing nscalatime. Using joda time directly since this app won't do much time manipulation

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,6 @@ import Keys._
 import play.Project._
 
 object ApplicationBuild extends Build {
-
   val appName         = "wallpaper"
   val appVersion      = "1.0-SNAPSHOT"
 
@@ -12,8 +11,8 @@ object ApplicationBuild extends Build {
     anorm,
     "mysql" % "mysql-connector-java" % "5.1.25",
     "org.mindrot" % "jbcrypt" % "0.3m",
-    "org.joda" % "joda-convert" % "1.2",
-    "com.github.nscala-time" %% "nscala-time" % "0.4.2"
+    "joda-time" % "joda-time" % "2.1",
+    "org.joda" % "joda-convert" % "1.2"
   )
 
   val compassTask = TaskKey[Unit]("compass", "Compile sass")
