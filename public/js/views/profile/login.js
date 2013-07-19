0fe74b3c6f527801bbba94bb0edad69fa64f4b81
Nick Adams <nadams@node-3.net>
2013-07-19 07:40:43 -0600

    Creating javascript for login page

---
+++ public/js/views/profile/login.js
@@ -0,0 +1,15 @@
+/* global ko */
+
+var wallpaper = wallpaper || { };
+wallpaper.profile = wallpaper.profile || { };
+wallpaper.profile.login = wallpaper.profile.login || { };
+
+wallpaper.profile.login.Model = (function (ko) {
+  "use strict";
+
+  var Model = function(data) {
+
+  };
+
+  return Model;
+}(ko));
